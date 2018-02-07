b1134b73aefaf05472683defaaa3113fe8ecf6ef
lynxz <lynxz8866@gmail.com>
2018-02-07 13:57:32 +0800

    完善弧形进度条

--- app/src/main/java/lynxz/org/kotlinapplication/activity/ArcProgressActivity.kt
+++ app/src/main/java/lynxz/org/kotlinapplication/activity/ArcProgressActivity.kt
@@ -2,6 +2,7 @@ package lynxz.org.kotlinapplication.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_arc_progress.*
 import lynxz.org.kotlinapplication.R
 
 /**
@@ -12,5 +13,8 @@ class ArcProgressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arc_progress)
+
+        tv_forward_progress.setOnClickListener { ap_demo.setProgress(ap_demo.getProgress() + 1) }
+        tv_backward_progress.setOnClickListener { ap_demo.setProgress(ap_demo.getProgress() - 1) }
     }
 }
--- app/src/main/java/lynxz/org/kotlinapplication/widget/ArcProgress.kt
+++ app/src/main/java/lynxz/org/kotlinapplication/widget/ArcProgress.kt
@@ -14,18 +14,21 @@ import kotlin.math.min
 /**
  * Created by lynxz on 29/01/2018.
  * 博客: https://juejin.im/user/5812c2b0570c3500605a15ff
+ *
+ * 用于绘制带刻度的进度条圆弧(上半圆弧)
  */
 class ArcProgress(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
     private var foreColor = Color.GREEN // 进度前景色
     private var bgColor = Color.LTGRAY //背景色
     private var pointColor = Color.DKGRAY
-    private var startAngle = -140f // 圆弧起始角度
-    private var sweepAngle = 100f // 圆弧扫过的角度
-    private var barWidth = 8f
-    private var currentProgress = 0f // 当前已完成的进度 0-100
-    private var pointRadius = 4f
-    private var pointCount = 3
+    private var startAngle = -0f // 圆弧起始角度
+    private var sweepAngle = 0f // 圆弧扫过的角度
+    private var barWidth = 0f
+    private var currentProgress = 0 // 当前已完成的进度 0-pointCount
+    private var pointCount = 0 // 总进度等分数
+    private var pointRadius = 0f
+    private var ovalRect = RectF(0f, 0f, 600f, 600f)
 
     init {
         context.theme.obtainStyledAttributes(attrs, R.styleable.ArcProgress, 0, 0).let {
@@ -34,13 +37,29 @@ class ArcProgress(context: Context, attrs: AttributeSet?) : View(context, attrs)
             pointColor = it.getColor(R.styleable.ArcProgress_point_color, Color.DKGRAY)
             startAngle = it.getFloat(R.styleable.ArcProgress_start_angle, -140f)
             sweepAngle = it.getFloat(R.styleable.ArcProgress_sweep_angle, 100f)
-            barWidth = it.getDimension(R.styleable.ArcProgress_bar_width, 8f)
-            currentProgress = it.getFloat(R.styleable.ArcProgress_progress, 0f)
-            pointCount = it.getInt(R.styleable.ArcProgress_progress_count, 3)
-            pointRadius = barWidth / 3
-//            Logger.d("barWidth = $barWidth")
+            barWidth = it.getDimension(R.styleable.ArcProgress_bar_width, 10f)
+            currentProgress = it.getInt(R.styleable.ArcProgress_progress, 0)
+            pointCount = it.getInt(R.styleable.ArcProgress_progress_count, 0)
+            pointRadius = barWidth / 4
+
+            pointCount = if (pointCount >= 100) 100 else pointCount
             it.recycle()
         }
+    }
+
+    /**
+     * 设置进度
+     * */
+    fun setProgress(newProgress: Int) {
+        currentProgress = newProgress
+        invalidate()
+    }
+
+    /**
+     * 获取当前进度
+     * */
+    fun getProgress(): Int {
+        return currentProgress
     }
 
     private val mBgPaint: Paint by lazy {
@@ -50,7 +69,6 @@ class ArcProgress(context: Context, attrs: AttributeSet?) : View(context, attrs)
             strokeWidth = barWidth
         }
     }
-
 
     private val mForePaint: Paint by lazy {
         Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -62,19 +80,20 @@ class ArcProgress(context: Context, attrs: AttributeSet?) : View(context, attrs)
 
     private val mPointPaint: Paint by lazy {
         Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            style = Paint.Style.STROKE
+            style = Paint.Style.FILL_AND_STROKE
             color = pointColor
             strokeWidth = pointRadius
         }
     }
 
 
-    private var ovalRect = RectF(0f, 0f, 600f, 600f)
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-////        ovalRect = RectF(left.toFloat(), top.toFloat() + barWidth / 2,
-////                right.toFloat(), bottom.toFloat() - barWidth / 2)
-////        Logger.d(ovalRect.toShortString())
+        // 若定义的 RectF 不是正方形, 则 canvas.drawArc(...) 时, 绘制出来的 startAngle 与传入值不匹配, 会有偏差, 原因不明
+//        ovalRect = RectF(left.toFloat(), top.toFloat() + barWidth / 2,
+//                right.toFloat(), bottom.toFloat() - barWidth / 2)
+//        Logger.d(ovalRect.toShortString())
+
         val rectLeft = barWidth / 2
         val rectTop = barWidth / 2
         val ovalWidth = min(width, height)
@@ -82,54 +101,54 @@ class ArcProgress(context: Context, attrs: AttributeSet?) : View(context, attrs)
         ovalRect = RectF(left.toFloat(), top.toFloat() + barWidth / 2,
                 rectLeft + ovalWidth, rectTop + ovalWidth - barWidth / 2)
     }
-
-    private fun getRect() {
-        val rectLeft = barWidth / 2
-        val rectTop = barWidth / 2
-        val ovalWidth = min(width, height)
-
-        ovalRect = RectF(left.toFloat(), top.toFloat() + barWidth / 2,
-                rectLeft + ovalWidth, rectTop + ovalWidth - barWidth / 2)
-    }
-
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        getRect()
+        if (pointCount in 2..currentProgress) {
+            currentProgress = pointCount
+        } else if (currentProgress < 0) {
+            currentProgress = 0
+        }
+
         canvas?.apply {
-            //            drawRect(ovalRect, mForePaint)
             Logger.d("startAngle = $startAngle ,sweepAngle = $sweepAngle")
-            drawArc(ovalRect, startAngle, sweepAngle, true, mBgPaint)
-            drawRect(ovalRect, mBgPaint)
-//            drawArc(ovalRect, -140f, 100f, true, mBgPaint)
+            drawArc(ovalRect, startAngle, sweepAngle, false, mBgPaint)
+
+            var perAngle = sweepAngle / 100
+
+            if (pointCount in 2..99) {
+                perAngle = sweepAngle / pointCount
+            }
+
             if (currentProgress in 0..100) {
-                val sweepProgressAngle = sweepAngle / 100 * currentProgress
-//                drawArc(ovalRect, startAngle, sweepProgressAngle, true, mForePaint)
+                var sweepProgressAngle = perAngle * currentProgress
+                if (sweepProgressAngle > sweepAngle) {
+                    sweepProgressAngle = sweepAngle
+                }
+                drawArc(ovalRect, startAngle, sweepProgressAngle, false, mForePaint)
             }
 
             if (pointCount in 2..99) {
                 val a = ovalRect.width() / 2 - barWidth + pointRadius
                 val b = ovalRect.height() / 2 - barWidth + pointRadius
-                val perAngle = sweepAngle / pointCount
 
-                for (i in 0..pointCount) {
+
+                for (i in 1 until pointCount) {
                     val angle = (startAngle + perAngle * i) % 360
                     val angleR = angle * Math.PI / 180
 
                     val tan = Math.tan(angleR)
                     var x = Math.sqrt(a * a * b * b / (b * b + a * a * tan * tan))
                     var y = tan * x
-                    if (angle <= -90) {
+                    if (angle <= -90 || angle in 180..270) {
                         x = -x
                         y = -y
                     }
 
                     val actX = x + ovalRect.width() / 2 + ovalRect.left
-                    val actY = y + ovalRect.height() / 2 + ovalRect.top
+                    val actY = y + ovalRect.height() / 2 + ovalRect.top - pointRadius * 2
 
-                    // 1440 1027
-                    Logger.d(" $angle $tan $x ... $y  actY = $actY")
                     drawCircle(actX.toFloat(), actY.toFloat(), pointRadius, mPointPaint)
                 }
             }
